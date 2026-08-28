555d61c461c23a543832f705015d4741b2ff73c4
王宝乐 <1787111257@qq.com>
2026-08-28 12:21:27 +0800

    style: narrow sidebar width

--- frontend/src/components/layout/AppSidebar.vue
+++ frontend/src/components/layout/AppSidebar.vue
@@ -2,7 +2,7 @@
   <aside
     class="sidebar"
     :class="[
-      sidebarCollapsed ? 'w-[72px]' : 'w-64',
+      sidebarCollapsed ? 'w-[72px]' : 'w-[15.25rem]',
       { '-translate-x-full lg:translate-x-0': !mobileOpen }
     ]"
   >
