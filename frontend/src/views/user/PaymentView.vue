ede6a09dfeeb5ee7abe88facde44e445d0b3d393
王宝乐 <1787111257@qq.com>
2026-09-01 19:29:27 +0800

    feat(payment): simplify quick top-up amounts

--- frontend/src/views/user/PaymentView.vue
+++ frontend/src/views/user/PaymentView.vue
@@ -59,7 +59,7 @@
               <section class="card p-5 sm:p-6">
                 <AmountInput
                   v-model="amount"
-                  :amounts="[10, 20, 50, 100, 200, 500, 1000, 2000, 5000]"
+                  :amounts="[20, 50, 100, 200, 500, 1000]"
                   :min="globalMinAmount"
                   :max="globalMaxAmount"
                   :currency-symbol="currencySymbol(selectedCurrency)"
