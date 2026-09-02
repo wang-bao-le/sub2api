750376e28b5bf0283273a7fdbda95994d3d3b7a1
王宝乐 <1787111257@qq.com>
2026-09-02 16:57:36 +0800

    chore: rename sidebar docs label

--- frontend/src/i18n/locales/zh/common.ts
+++ frontend/src/i18n/locales/zh/common.ts
@@ -196,7 +196,7 @@ export default {
     github: 'GitHub',
     mySubscriptions: '我的订阅',
     buySubscription: '充值/订阅',
-    docs: '文档',
+    docs: '文档中心',
     myOrders: '我的订单',
     orderManagement: '订单管理',
     paymentDashboard: '支付概览',
